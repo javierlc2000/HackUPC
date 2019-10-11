617b14721749aa12ad4fcff4a984edb758e49d02
Xavier Povill <xpovilltrplds@gmail.com>
2019-10-12 01:25:57 +0200

    Add teacher

--- api/subject.go
+++ api/subject.go
@@ -6,6 +6,7 @@ import (
 
 type subject struct {
   name string
+  teacher string
   schedule [7] bool
   start_hour [7] int
   start_min [7] int
@@ -13,7 +14,7 @@ type subject struct {
   end_min [7] int
 }
 
-func NewSubject (name string, schedule [7] bool, start_hour, start_min, end_hour, end_min [7] int) subject {
-  s := subject {name, schedule, start_hour, start_min, end_hour, end_min}
+func NewSubject (name, teacher string, schedule [7] bool, start_hour, start_min, end_hour, end_min [7] int) subject {
+  s := subject {name, teacher, schedule, start_hour, start_min, end_hour, end_min}
   return s
 }
