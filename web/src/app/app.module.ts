6f04b17ab8d50bf643080d1bf12087499e0b6fc0
Javier López-Contreras <38789151+javierlc2000@users.noreply.github.com>
2019-10-11 21:55:48 +0200

    Add Angular routing
    Add initial-page unimplemented components

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,19 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginBoxComponent } from './initial-page/login-box/login-box.component';
+import { RegisterBoxComponent } from './initial-page/register-box/register-box.component';
+import { CentralContainerComponent } from './initial-page/central-container/central-container.component';
+import { HeaderComponent } from './initial-page/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginBoxComponent,
+    RegisterBoxComponent,
+    CentralContainerComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    MatButtonModule, MatCardModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ web/src/app/initial-page/register-box/register-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-box',
+  templateUrl: './register-box.component.html',
+  styleUrls: ['./register-box.component.scss']
+})
+export class RegisterBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
